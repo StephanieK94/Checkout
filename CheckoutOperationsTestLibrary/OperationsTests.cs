2f9ef834d31776efb5f92603ac9fe9d6f8afe4b2
StephanieK <stephanie.kitchen@serko.com>
2019-07-09 11:52:46 +1200

    Initial commit setting up projects and test library

---
+++ CheckoutOperationsTestLibrary/OperationsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CheckoutOperationsTestLibrary
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CheckoutOperationsTestLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckoutOperationsTestLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckoutOperationsTestLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ce23697-f1de-42b7-9b32-18deb42c31db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
