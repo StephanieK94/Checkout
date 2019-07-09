8f6be42b7ce3c27110505fe338aa8287bc5f9a00
StephanieK <stephanie.kitchen@serko.com>
2019-07-09 13:44:36 +1200

    Test of product count returns single product

--- CheckoutOperationsTestLibrary/OperationsTests.cs
+++ CheckoutOperationsTestLibrary/OperationsTests.cs
@@ -20,23 +20,9 @@ namespace CheckoutOperationsTestLibrary
             Assert.AreEqual(1, productList.Count);
         }
 
-        //[Test]
-        //public void GivenFullProductDictionary_GetTotalCostOfPriceList()
-        //{
-        //    var productList = new Dictionary<char, int>();
-        //    productList.Add('A', 50);
-        //    productList.Add('B', 30);
-        //    productList.Add('C', 20);
-        //    productList.Add('D', 15);
-
-        //    var checkoutOperator = new Operator();
-        //    int totalPrice = checkoutOperator.GetCustomerTotal(productList);
-
-        //    Assert.AreEqual(115, totalPrice);
-        //}
 
         [Test]
-        public void GivenCustomerListOfOneProdcut_GetTotalCustomerPrice()
+        public void GivenCustomerListOfOneProduct_GetTotalCustomerPrice()
         {
             var productList = new Dictionary<char, int>();
             productList.Add('A', 50);
@@ -53,6 +39,68 @@ namespace CheckoutOperationsTestLibrary
             int customerTotal = checkoutOperator.GetCustomerTotal(productList, customerPurchasesList);
 
             Assert.AreEqual(50, customerTotal);
+        }
+
+        [Test]
+        public void GivenCustomerListOfTwoDifferentProducts_GetTotalCustomerPriceOf50()
+        {
+            var productList = new Dictionary<char, int>();
+            productList.Add('A', 50);
+            productList.Add('B', 30);
+            productList.Add('C', 20);
+            productList.Add('D', 15);
+
+            var customerPurchasesList = new List<char>()
+            {
+                'A',
+                'B',
+            };
+
+            var checkoutOperator = new Operator();
+            int customerTotal = checkoutOperator.GetCustomerTotal(productList, customerPurchasesList);
+
+            Assert.AreEqual(80, customerTotal);
+        }
+
+        [Test]
+        public void GivenCustomerListOfTwoOfSameProducts_GetTotalCustomerPriceOf100()
+        {
+            var productList = new Dictionary<char, int>();
+            productList.Add('A', 50);
+            productList.Add('B', 30);
+            productList.Add('C', 20);
+            productList.Add('D', 15);
+
+            var customerPurchasesList = new List<char>()
+            {
+                'A',
+                'A',
+            };
+
+            var checkoutOperator = new Operator();
+            int customerTotal = checkoutOperator.GetCustomerTotal(productList, customerPurchasesList);
+
+            Assert.AreEqual(100, customerTotal);
+        }
+
+        [Test]
+        public void GivenCustomerPurchaseList_ReturnsCountOfSingleProduct()
+        {
+            var productList = new Dictionary<char, int>();
+            productList.Add('A', 50);
+            productList.Add('B', 30);
+            productList.Add('C', 20);
+            productList.Add('D', 15);
+
+            var customerPurchasesList = new List<char>()
+            {
+                'A',
+            };
+
+            var checkoutOperator = new Operator();
+            int countOfProduct = checkoutOperator.GetCountOfSingleProduct(customerPurchasesList);
+
+            Assert.AreEqual(1, countOfProduct);
         }
     }
 }
